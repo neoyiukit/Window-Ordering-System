48626bfc8a3b8f949af9816dfea748dbd7b11d59
Neo Yiu <neo.yiu@rakuten.com>
2017-08-21 17:21:51 +0100

    renamed the object of OrderGenerator and cleaned a bit of the test suites

--- src/test/java/com/oocode/MainTest.java
+++ src/test/java/com/oocode/MainTest.java
@@ -7,55 +7,41 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertEquals;
 
+// do not run without changing "Ivan's Windows" in com.oocode.BodyBuilder to "test"
+// otherwise you have to pay for the order
+
 public class MainTest {
 
-    OrderGenerator orderGlass;
+    OrderGenerator newOrder;
 //    RequestBody mockRequestBody = mock(RequestBody.class);
 //    Request mockRequest = mock(Request.class);
 
     @Test
     public void testIfTheInputMatchedFieldsInOrderGlass() throws Exception {
-        // do not run without changing "Ivan's Windows" in com.oocode.BodyBuilder to "test"
-        // otherwise you have to pay for the order
 
         // When
-        orderGlass = new OrderGenerator(50, 10, 10, "Churchill");
-        orderGlass.orderDetermination();
-//        System.out.println(orderGlass.ReturnWidthThicknessAllowance(orderGlass.getModelName(), true));
-//        System.out.println(orderGlass.ReturnWidthThicknessAllowance(orderGlass.getModelName(), false));
-//        System.out.println(orderGlass.getWindowType());
-//        int widthThicknessAllowance = ReturnThicknessAllowance.ReturnWidthThicknessAllowance(orderGlass.getModelName());
-//        int heightThicknessAllowance = ReturnThicknessAllowance.ReturnHeightThicknessAllowance(orderGlass.getModelName());
-//        int total = orderGlass.getCalculatedTotal(orderGlass.getWidthOfWindow(), orderGlass.getHeightOfWindow(), orderGlass.getNumberOfWindow(), widthThicknessAllowance, heightThicknessAllowance);
-//        System.out.println(total);
+        newOrder = new OrderGenerator(50, 10, 10, "Churchill");
+        newOrder.orderDetermination();
 
         // Then
-        assertEquals("Unmatched Width of Window Passed", 50, orderGlass.getWidthOfWindow());
-        assertEquals("Unmatched Height of Window Passed", 10, orderGlass.getHeightOfWindow());
-        assertEquals("Unmatched Number of Window Passed", 10, orderGlass.getNumberOfWindow());
-        assertEquals("Unmatched Window Model", "Churchill", orderGlass.getModelName());
-        assertEquals("Unmatched Width Thickness Allowance", 4, ThicknessAllowanceHelper.ReturnWidthThicknessAllowance(orderGlass.getModelName()));
-        assertEquals("Unmatched Height Thickness Allowance", 3, ThicknessAllowanceHelper.ReturnHeightThicknessAllowance(orderGlass.getModelName()));
-        assertEquals("Unmatched Window Type", "toughened", orderGlass.getWindowType());
-
-
-//        main(new String[]{"123", "456", "789", "Churchill"});
-//        main(new String[]{"48", "36", "1", "Victoria"});
-        /* EXPECTED OUTPUT
-Thank you "test" for your large order (q=789, w=119, h=453, toughened). Order not really placed - nothing to pay
-Thank you "test" for your order (q=1, w=46, h=33, plain). Order not really placed - nothing to pay
-         */
+        assertEquals("Unmatched Width of Window Passed", 50, newOrder.getWidthOfWindow());
+        assertEquals("Unmatched Height of Window Passed", 10, newOrder.getHeightOfWindow());
+        assertEquals("Unmatched Number of Window Passed", 10, newOrder.getNumberOfWindow());
+        assertEquals("Unmatched Window Model", "Churchill", newOrder.getModelName());
+        assertEquals("Unmatched Width Thickness Allowance", 4, ThicknessAllowanceHelper.ReturnWidthThicknessAllowance(newOrder.getModelName()));
+        assertEquals("Unmatched Height Thickness Allowance", 3, ThicknessAllowanceHelper.ReturnHeightThicknessAllowance(newOrder.getModelName()));
+        assertEquals("Unmatched Window Type", "toughened", newOrder.getWindowType());
     }
 
     @Test
     public void testIfCallsLargeOrderEndpoint() throws Exception{
 
         // when
-        orderGlass = new OrderGenerator(123, 456, 789, "Churchill");
-        orderGlass.orderDetermination();
+        newOrder = new OrderGenerator(123, 456, 789, "Churchill");
+        newOrder.orderDetermination();
 
         // Then
-        assertThat("Total Area is smaller than 20000", orderGlass.getCalculatedTotal(), greaterThan(20000));
+        assertThat("Total Area is smaller than 20000", newOrder.getCalculatedTotal(), greaterThan(20000));
         // assertTrue("Total Area is smaller than 20000", total > 20000);
         // TODO: Make sure to compare the url if it goes to the large endpoint
     }
@@ -64,11 +50,11 @@ Thank you "test" for your order (q=1, w=46, h=33, plain). Order not really place
     public void testIfCallsSmallerOrderEndpoint() throws Exception{
 
         // when
-        orderGlass = new OrderGenerator(2, 2, 2, "Churchill");
-        orderGlass.orderDetermination();
+        newOrder = new OrderGenerator(2, 2, 2, "Churchill");
+        newOrder.orderDetermination();
 
         // Then
-        assertThat("Total Area is larger than 20000", orderGlass.getCalculatedTotal(), lessThanOrEqualTo(20000));
+        assertThat("Total Area is larger than 20000", newOrder.getCalculatedTotal(), lessThanOrEqualTo(20000));
          // assertTrue("Total Area is larger than 20000", total < 20000);
          // TODO: Make sure to compare the url if it goes to the small endpoint
     }
