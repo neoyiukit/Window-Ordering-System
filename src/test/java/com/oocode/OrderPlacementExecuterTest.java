7462d0b4ad7a78ea620fe9eec1e69683ba170407
Neo Yiu <neo.yiu@rakuten.com>
2017-08-29 03:30:07 +0100

    added the test case of whether RequestBodyBuilder could be skipped

--- src/test/java/com/oocode/OrderPlacementExecuterTest.java
+++ src/test/java/com/oocode/OrderPlacementExecuterTest.java
@@ -22,6 +22,7 @@ public class OrderPlacementExecuterTest {
 
         // GIVEN
         OrderInfoHandler orderInfoHandler = new OrderInfoHandler(5, 119, 1000, "Churchill", "test");
+        OrderRequestsWrapper orderRequestsWrapper = new OrderRequestsWrapper(orderInfoHandler);
         RequestBodyBuilder mockRequestBodyBuilder = mock(RequestBodyBuilder.class);
         RequestBody requestBody = new RequestBody() { // requestBody couldn't be instantiated unless the RequestBody data type is not "abstract" in nature meaning all the constituent methods must not be abstract
             @Override
@@ -39,9 +40,40 @@ public class OrderPlacementExecuterTest {
 
         // WHEN
         when(mockRequestBodyBuilder.bodyBuilderForAnyOrders(anyInt(), anyInt(), anyInt(), anyInt(), anyInt(), anyString(), anyString())).thenReturn(requestBody);
+        OrderPlacementExecuter.orderRequestGenerator(orderInfoHandler, orderRequestsWrapper);
 
         // THEN
         Mockito.verify(mockRequestBodyBuilder, atLeastOnce()).bodyBuilderForAnyOrders(OrderInfoHandler.getWidthOfWindow(), OrderInfoHandler.getHeightOfWindow(), OrderInfoHandler.getNumberOfWindow(), OrderInfoHandler.getWidthThicknessAllowance(), OrderInfoHandler.getHeightThicknessAllowance(), OrderInfoHandler.getGlassType(), OrderInfoHandler.getUserName());
+        assertEquals(orderInfoHandler.getRequestBody(), requestBody);
+    }
+
+    @Test(expected = org.mockito.exceptions.misusing.InvalidUseOfMatchersException.class)
+    public void testingIfRequestBodyBuilderMethodCouldBeSkipped() throws Exception{
+
+        // GIVEN
+        OrderInfoHandler orderInfoHandler = new OrderInfoHandler(5, 119, 1000, "Churchill", "test");
+        OrderRequestsWrapper orderRequestsWrapper = new OrderRequestsWrapper(orderInfoHandler);
+        RequestBodyBuilder mockRequestBodyBuilder = mock(RequestBodyBuilder.class);
+        RequestBody requestBody = new RequestBody() { // requestBody couldn't be instantiated unless the RequestBody data type is not "abstract" in nature meaning all the constituent methods must not be abstract
+            @Override
+            public MediaType contentType() {
+                // there is an abstract method of contentType() and should be overridden with dummy "return mediaType" which is null
+                MediaType mediaType = null;
+                return mediaType;
+            }
+
+            @Override
+            public void writeTo(BufferedSink sink) throws IOException {
+                // there is an abstract method of writeTo() so it should be overridden with empty content to get rid of the abstract status
+            }
+        };
+
+        // WHEN
+        when(mockRequestBodyBuilder.bodyBuilderForAnyOrders(anyInt(), anyInt(), anyInt(), anyInt(), anyInt(), anyString(), anyString())).thenReturn(requestBody);
+        OrderPlacementExecuter.orderRequestGenerator(orderInfoHandler, orderRequestsWrapper);
+
+        // THEN
+        Mockito.verify(mockRequestBodyBuilder, times(0)).bodyBuilderForAnyOrders(OrderInfoHandler.getWidthOfWindow(), OrderInfoHandler.getHeightOfWindow(), OrderInfoHandler.getNumberOfWindow(), OrderInfoHandler.getWidthThicknessAllowance(), OrderInfoHandler.getHeightThicknessAllowance(), OrderInfoHandler.getGlassType(), OrderInfoHandler.getUserName());
         assertEquals(orderInfoHandler.getRequestBody(), requestBody);
     }
 }
