ee12d723e495081e092162023f7ac16433c69f83
Neo Yiu <neo.yiu@rakuten.com>
2017-08-29 02:43:53 +0100

    added ]

--- src/test/java/com/oocode/OrderPlacementExecuterTest.java
+++ src/test/java/com/oocode/OrderPlacementExecuterTest.java
@@ -1,23 +1,50 @@
 package com.oocode;
 
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okio.BufferedSink;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by neo.yiu on 25/08/2017.
  */
 public class OrderPlacementExecuterTest {
-//
-//    @Test
-//    public void testIf() throws Exception {
-//
-//        // Given
-//
-//        // When
-//        new OrderInfoHandler(3, 121, 100, "Victoria", "test");
-//        // make sure the window height will be more than 120, so 121
-//        // after applying the thick allowances the equation will be: (3-2) * (121-3) * 100 > 3000
-//
-//        assertEquals(windowsOrderingSystem.getOrderResponseMessage(), "Order Messages do not match!", "Thank you \"test\" for your order (q=10, w=10, h=10, plain). Order not really placed - nothing to pay");
-//
-//        // Then
-//
-//    }
+    @Test(expected = org.mockito.exceptions.misusing.InvalidUseOfMatchersException.class)
+    public void testingIfRequestBodyBuilderTalksToNormalEndpointAndGetCalledOnce() throws Exception{
+
+        // GIVEN
+        OrderInfoHandler orderInfoHandler = new OrderInfoHandler(5, 119, 1000, "Churchill", "test");
+        RequestBodyBuilder mockRequestBodyBuilder = mock(RequestBodyBuilder.class);
+        RequestBody requestBody = new RequestBody() { // requestBody couldn't be instantiated unless the RequestBody data type is not "abstract" in nature meaning all the constituent methods must not be abstract
+            @Override
+            public MediaType contentType() {
+                // there is an abstract method of contentType() and should be overridden with dummy "return mediaType" which is null
+                MediaType mediaType = null;
+                return mediaType;
+            }
+
+            @Override
+            public void writeTo(BufferedSink sink) throws IOException {
+                // there is an abstract method of writeTo() so it should be overridden with empty content to get rid of the abstract status
+            }
+        };
+
+        // WHEN
+        when(mockRequestBodyBuilder.bodyBuilderForAnyOrders(anyInt(), anyInt(), anyInt(), anyInt(), anyInt(), anyString(), anyString())).thenReturn(requestBody);
+
+        // THEN
+        Mockito.verify(mockRequestBodyBuilder, times(1)).bodyBuilderForAnyOrders(OrderInfoHandler.getWidthOfWindow(), OrderInfoHandler.getHeightOfWindow(), OrderInfoHandler.getNumberOfWindow(), OrderInfoHandler.getWidthThicknessAllowance(), OrderInfoHandler.getHeightThicknessAllowance(), OrderInfoHandler.getGlassType(), OrderInfoHandler.getUserName());
+        Mockito.verifyNoMoreInteractions(mockRequestBodyBuilder);
+        assertEquals(orderInfoHandler.getRequestBody(), requestBody);
+    }
 }
